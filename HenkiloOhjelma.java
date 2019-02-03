9cc9ef0e6678327f6323e5357ad4bd2a37642211
JoonasPit <43230082+JoonasPit@users.noreply.github.com>
2019-02-03 08:27:29 +0200

    Create HenkiloOhjelma.java
    main of Henkilo

---
+++ HenkiloOhjelma.java
@@ -0,0 +1,27 @@
+package kotitehtäviä;
+import java.util.Scanner;
+
+public class HenkiloOhjelma {
+
+	public static void main(String[] args) {
+		//Define variables for main program
+		String UserName;
+		String UserAddress;
+		// Introduce scanner
+		Scanner input = new Scanner(System.in);
+		//Call my other class and create function
+		Henkilo test = new Henkilo();
+		//Set variables
+		System.out.print("Anna nimi: ");
+		UserName = input.nextLine();
+		test.setNimi(UserName);
+		
+		System.out.print("Anna osoite: ");
+		UserAddress = input.nextLine();
+		test.setOsoite(UserAddress);
+
+		System.out.println("Nimi on: " + test.getNimi() +  " | Osoite on: " + test.getOsoite());
+		System.out.print(test.toString());
+	}
+
+}
