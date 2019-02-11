c596efdb1d3552c0bbe2079c90a1560d98eb218f
JoonasPit <43230082+JoonasPit@users.noreply.github.com>
2019-02-11 14:38:44 +0200

    Update HenkiloSovellus.java

--- HenkiloSovellus.java
+++ HenkiloSovellus.java
@@ -1,42 +1,59 @@
-package lisääläksyjä;
 import java.util.Scanner;
 
 public class HenkiloSovellus {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		Henkilo create = new Henkilo();
 		
 		boolean loop = true;
-		
-		while(loop == true) {
-			int userselect = 0;
+		int userselect = 0;
+		String userName = "";
+		String userAddress = "";
+		while (loop == true){
+		System.out.println("1. Anna henkilö\n2. Näytä henkilön tiedot\n3."
+				+ " Muuta henkilön tietoja\n0. Lopetus");
 			
-			System.out.print("Anna valinta 1-3");
-			userselect = input.nextInt();
-			if(userselect == 0){
-				loop = false;
-			}
-			
+		System.out.print("Anna valintasi (0-3): ");
+		userselect = input.nextInt();
+		if (userselect == 1) {
+			System.out.print("Anna nimi: ");
+			userName = input.nextLine();
+			userName = input.nextLine();
+			create.setNimi(userName);
+			System.out.print("Anna osoite: ");
+			userAddress = input.nextLine();
+			create.setOsoite(userAddress);
+			continue;
 		}
-		
-		// kesken!!!!!
-		
-		// looppi ohjaa aina oikeaan ohjelmaan. 
-		// if 1 
-		// inputit to set.nimi
-		// inputit to set.osoite
-		
-		// if 2 
-		
-		// muokkaus from arrayspot
-		// set arrayvar
-		// change input at array[arrayvar]
-		
-		// muunna nimi & osoite Arrayksi @ Henkilo.java
-		
-		// toString for loop tulostus ??
-		
-		//printtaa tostring
+		else if (userselect == 2) {
+			if(userName == "" && userAddress == "") {
+				System.out.println("Henkilöä ei ole!");
+				continue;
+			}
+			System.out.println(create.toString());
+		}
+		else if (userselect == 3) {
+			if(userName == "" && userAddress == "") {
+				System.out.println("Henkilöä ei ole!");
+				continue;
+			}
+			System.out.print("Anna nimi: ");
+			userName = input.nextLine();
+			userName = input.nextLine();
+			create.setNimi(userName);
+			System.out.print("Anna osoite: ");
+			userAddress = input.nextLine();
+			create.setOsoite(userAddress);
+			continue;
+		}
+		else if (userselect == 0){
+			loop = false;
+			break;
+		}
+		else {
+			break;
+		}
+		}
 	}
-
 }
